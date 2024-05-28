587c33398ead03ac282677a74e905f60975df9b4
Luke Leiby <lucaswleiby@gmail.com>
2024-05-27 21:33:02 -0400

    Use proper error handling in export plugins script

--- exportPlugins.py
+++ exportPlugins.py
@@ -2,20 +2,22 @@ import subprocess
 
 
 def exportPlugins():
+	print('Exporting plugins...', end='', flush=True)
+
 	command = 'docker compose cp wordpress:/var/www/html/wp-content/plugins ./wp-content'
 
 	try:
 		result = subprocess.run(command, check=True, capture_output=True, text=True)
-	except subprocess.CalledProcessUrlError as e:
-		print(f"Command '{e.cmd}' returned non-zero exit status {e.returncode}")
+	except subprocess.CalledProcessError as e:
+		print(f"\nCommand '{e.cmd}' returned non-zero exit status {e.returncode}")
 		print(f'Error output: {e.stderr}')
 		raise
 
+	print('Done')
+
 
 def main():
-	print('Exporting plugins...', end='', flush=True)
 	exportPlugins()
-	print('Done')
 
 
 if __name__ == '__main__':
